d16478f67aceb062f74498dbe1d230e90116f79a
ricardossiqueira <ricardosantossiqueira@poli.ufrj.br>
2022-06-09 13:58:14 -0300

    ⚙️basic chakra and api config

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+import { theme } from "../styles/theme";
+import { queryClient } from "../services/queryClient";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp;
